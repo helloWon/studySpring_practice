a5f85b5083deb545fbc623d4cc572bef76ab45b7
helloWon <leeseongwon13@gmail.com>
2022-03-27 13:41:15 +0900

    add domain 'Member'

---
+++ practice/src/main/java/com/example/practice/domain/member/Member.java
@@ -0,0 +1,20 @@
+package com.example.practice.domain.member;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private Long id;
+    private String username;
+    private int age;
+
+    public Member(String username, int age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public Member() {
+
+    }
+
+}
