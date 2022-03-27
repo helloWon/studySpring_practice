0e9a251c03e159e76bbf5d605a8eb78f654719c5
helloWon <leeseongwon13@gmail.com>
2022-03-27 15:08:06 +0900

    recreate project

--- springmvc/src/main/java/practice/springmvc/SpringmvcApplication.java
+++ springmvc/src/main/java/practice/springmvc/SpringmvcApplication.java
@@ -1,13 +1,13 @@
-package com.example.practice;
+package practice.springmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PracticeApplication {
+public class SpringmvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PracticeApplication.class, args);
+		SpringApplication.run(SpringmvcApplication.class, args);
 	}
 
 }
--- springmvc/src/test/java/practice/springmvc/SpringmvcApplicationTests.java
+++ springmvc/src/test/java/practice/springmvc/SpringmvcApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.practice;
+package practice.springmvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PracticeApplicationTests {
+class SpringmvcApplicationTests {
 
 	@Test
 	void contextLoads() {
