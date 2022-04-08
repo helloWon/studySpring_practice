527f9e78d6e619472a7b1641908cb3cd393e492a
helloWon <leeseongwon13@gmail.com>
2022-04-09 02:42:19 +0900

    Forms & Controllers

---
+++ springjpa/src/main/java/demo/springjpa/controller/MovieForm.java
@@ -0,0 +1,16 @@
+package demo.springjpa.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MovieForm {
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    private String director;
+    private String actor;
+}
